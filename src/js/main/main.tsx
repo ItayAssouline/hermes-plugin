b1882af872dd1d29444691e359d4c48a50c09625
Itay Assouline <fushionartz@gmail.com>
2023-09-16 01:35:36 +0300

    reading srt, and loading it into premiere

--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -1,8 +1,14 @@
+import axios from "axios";
 import path from "path";
 import { useEffect, useState } from "react";
 import { evalTS, subscribeBackgroundColor } from "../lib/utils/bolt";
+import FormData from "form-data";
 
 import "./main.scss";
+import { fs } from "../lib/cep/node";
+import { a } from "@react-spring/three";
+import { AssistantDirection } from "@mui/icons-material";
+import { nanoid } from "nanoid";
 
 const Main = () => {
   const [bgColor, setBgColor] = useState("#282c34");
@@ -18,7 +24,34 @@ const Main = () => {
       const outPath = path.join(__dirname, "/temp-files/temp.mp3");
       //todo: make sure if path exists, if not - create folder
 
-      const res = await evalTS("transcribe", outPath);
+      const sequenceInOut = await evalTS("exportInOutToMP3", outPath);
+
+      const formData = new FormData();
+
+      const fileData = fs.readFileSync(outPath);
+      formData.append(
+        "file",
+        new Blob([fileData], { type: "application/octet-stream" }),
+        "file.mp3"
+      );
+
+      const translationResponse = await axios.post(
+        "http://localhost:5555/transcription/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      const srtPath = path.join(
+        __dirname,
+        `/temp-files/subtitles-${nanoid()}.srt`
+      );
+      fs.writeFileSync(srtPath, translationResponse.data);
+      await evalTS("createTitles", srtPath, sequenceInOut.inPoint);
+      // fs.unlinkSync(outPath);
     } catch (e) {
       console.log(`Error: ${e}`);
       //todo: handle error
--- src/jsx/ppro/ppro.ts
+++ src/jsx/ppro/ppro.ts
@@ -1,5 +1,5 @@
 import { ISequenceSettings } from "../../js/types";
-export const transcribe = (fileOutPath: string): ISequenceSettings => {
+export const exportInOutToMP3 = (fileOutPath: string): ISequenceSettings => {
   if (!app.project) {
     throw Error("No project open");
   }
@@ -21,4 +21,45 @@ export const transcribe = (fileOutPath: string): ISequenceSettings => {
   } catch (e) {
     throw e;
   }
+};
+
+export const createTitles = (srtPath: string, startTime: number) => {
+  // var activeSeq = app.project.activeSequence;
+  var project = app.project;
+  if (project) {
+    // Specify the path to your file. This needs to be an absolute path.
+    var filePath = srtPath; // for Windows
+
+    // Import the file into the project
+    project.importFiles([filePath], true, project.rootItem, false);
+
+    const projectItemsLength = app.project.rootItem.children.numItems;
+
+    let importedSubtitles = null;
+    const fileName = new File(srtPath).name;
+    for (let index = 0; index < projectItemsLength; index++) {
+      const item = app.project.rootItem.children[index];
+      if (item.name === fileName) {
+        importedSubtitles = item;
+        break;
+      }
+    }
+
+    if (!importedSubtitles) throw Error("Error in importing subtitles");
+
+    // @ts-ignore
+    app.project.activeSequence.createCaptionTrack(
+      importedSubtitles,
+      startTime,
+      // @ts-ignore
+      Sequence.CAPTION_FORMAT_SUBTITLE
+    );
+    return true;
+  }
+  return false;
+  // activeSeq.createCaptionTrack(
+  //   importedItem,
+  //   startAtTime,
+  //   Sequence.CAPTION_FORMAT_SUBTITLE
+  // );
 };
