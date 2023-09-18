264758ba9f3df3c0bd5b48dd89b1ba3d02733bb3
Itay Assouline <fushionartz@gmail.com>
2023-09-19 01:43:10 +0300

    added awesome circular bar

--- src/js/components/ProgressLoader/ProgressLoader.tsx
+++ src/js/components/ProgressLoader/ProgressLoader.tsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./progressLoader.css";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 const ProgressLoader = () => {
-  const [percent, setPercent] = useState(50);
+  const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setPercent((prev) => (prev += 10) % 100);
+    }, 1200);
+  }, []);
+
   return (
     <div className="progressLoaderContainer">
-      <div className="circle">75%</div>
+      <div className="circle">{percent}%</div>
 
       <div className="border">
         <CircularProgressbar
-          value={75}
+          value={percent}
           background={false}
           backgroundPadding={0}
-          strokeWidth={2}
+          strokeWidth={1}
           styles={buildStyles({
             trailColor: "transparent",
-            pathColor: "#0050C9",
-            strokeLinecap: "butt",
+            pathColor: `#0050C9`,
+            rotation: 0.5,
           })}
         />
       </div>
